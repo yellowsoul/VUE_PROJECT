57ae710a9282079b1da07e68da634c53a5d8b373
yellowsoul <517878555@qq.com>
2022-07-21 00:29:36 +0800

    P128 015-尚硅谷-后台管理系统-品牌列表展示

---
+++ vue-admin-template-master/src/api/product/tradeMark.js
@@ -0,0 +1,5 @@
+// 这个模块主要获取的是品牌管理的数据模块
+import request from '@/utils/request';
+// 获取品牌列表接口
+// 
+export const reqTradeMarkList = (page, limit) => request({ url: `/admin/product/baseTrademark/${page}/${limit}`, method: 'get' })
--- vue-admin-template-master/src/views/product/tradeMark/index.vue
+++ vue-admin-template-master/src/views/product/tradeMark/index.vue
@@ -10,12 +10,24 @@
       label:显示的标题
       width:对应列的宽度
       aling:标题的对齐方式
+      prop:对应列内容的字段名
+      注意1：elmentUI当中的table组件，展示的数据是以一列一列进行展示数据
     -->
-    <el-table style="width:100%" border>
-      <el-table-column prop="prop" label="序号" width="80px" align="center"></el-table-column>
-      <el-table-column prop="prop" label="品牌名称" width="width"></el-table-column>
-      <el-table-column prop="prop" label="品牌LOGO" width="width"></el-table-column>
-      <el-table-column prop="prop" label="操作" width="width"></el-table-column>
+    <el-table style="width:100%" border :data="list">
+
+      <el-table-column type="index" label="序号" width="80px" align="center"></el-table-column>
+      <el-table-column prop="tmName" label="品牌名称" width="width"></el-table-column>
+      <el-table-column prop="logoUrl" label="品牌LOGO" width="width">
+        <template slot-scope="{row,$index}">
+          <img :src="row.logoUrl" alt="" style="width:100px;height:100px;">
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="操作" width="width">
+        <template slot-scope="{row,$index}">
+          <el-button type="warning" icon="el-icon-edit" size="mini">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
     
     <!-- 
@@ -33,12 +45,14 @@
     -->
     <el-pagination
       style="margin-top:20px;text-align: center;"
-      :current-page="6"
-      :total="99"
-      :page-size="3"
+      :current-page="page"
+      :total="total"
+      :page-size="limit"
       :page-count="7"
       :page-sizes="[3, 5, 10]"
       layout="prev, pager, next, jumper,->,sizes, total"
+      @current-change="getPageList"
+      @size-change="sizeChange"
     >
     </el-pagination>
     
@@ -51,9 +65,45 @@
 export default {
   name: 'tradeMark',
   data() {
-    return {}
+    return {
+      // 代表分页器第几页
+      page:1,
+      // 当前页数展示数据条数
+      limit:3,
+      // 总共数据条数
+      total:0,
+      // 列表展示的数据
+      list:[]
+    }
   },
-  methods: {},
+  // 组件挂载完毕发请求
+  mounted(){
+    // 获取列表数据方法
+    this.getPageList();
+  },
+  methods:{
+    // 获取品牌列表的数据
+    async getPageList(pager = 1){
+      this.page = pager
+      // 解构出参数
+      const {page, limit} = this;
+      // 获取品牌列表的接口
+      // 当你向服务器发请求的时候，这个函数需要带参数，因此在data当中初始化两个字段，代表给服务器传递参数
+      let result = await this.$API.trademark.reqTradeMarkList(page, limit);
+      if(result.code == 200){
+        //分别是展示数据总条数与列表展示的数据
+        this.total = result.data.total;
+        this.list = result.data.records;
+      }
+    },
+
+    // 当分页器某一页需要展示数据条数发生变化的时候会触发
+    sizeChange(limit){
+      // 整理参数
+      this.limit = limit
+      this.getPageList();
+    }
+  }
 }
 </script>
 <style scoped>
