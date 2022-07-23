1c21a23afe2858ac78ff143994eeb3e9a2c291cb
yellowsoul <517878555@qq.com>
2022-07-24 01:08:31 +0800

    P136 023-尚硅谷-后台管理系统-三级联动完成

--- vue-admin-template-master/src/main.js
+++ vue-admin-template-master/src/main.js
@@ -37,9 +37,14 @@ Vue.config.productionTip = false
 // console.log(process.env)
 // 引入相关API请求接口
 import API from '@/api';
+// 引入组件，全局注册
+import CategorySelect from '@/components/CategorySelect';
+
 // 组件实例的原型的原型指向的是Vue.prototype
 // 任意组件可以使用API相关的接口
 Vue.prototype.$API = API;
+// 注册全局组件
+Vue.component(CategorySelect.name,CategorySelect);
 new Vue({
   el: '#app',
   router,
--- vue-admin-template-master/src/views/product/Attr/index.vue
+++ vue-admin-template-master/src/views/product/Attr/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    Attr
+    <el-card style="margin:20px 0;">
+      <category-select @getCategoryId="getCategoryId"></category-select>
+    </el-card>
+    <el-card>
+      
+    </el-card>
   </div>
 </template>
 
@@ -8,9 +13,37 @@
 export default {
   name:'Attr',
   data () {
-    return {}
+    return {
+      category1Id:'',
+      category2Id:'',
+      category3Id:''
+    }
   },
-  methods: {},
+  methods: {
+    // 自定义事件的回调
+    getCategoryId({categoryId,level}){
+      console.log(level)
+      // 区分三级分类相应的id，以及父组件进行存储
+      if(level == 1){
+        this.category1Id = categoryId;
+        this.category2Id = ''
+        this.category3Id = ''
+      }else if(level == 2){
+        this.category2Id = categoryId;
+        this.category3Id = ''
+      }else{
+        // 代表三级分类的id有了
+        this.category3Id = categoryId;
+        // 发请求获取品牌属性
+        this.getAttrList();
+      }
+    },
+
+    // 获取平台属性的数据
+    getAttrList(){
+      console.log('发请求')
+    },
+  },
 }
 </script>
 
