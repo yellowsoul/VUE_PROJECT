3341006d9f822a64472581d66b6a758155825af5
yellowsoul <517878555@qq.com>
2022-07-24 17:06:19 +0800

    P137 024-尚硅谷-后台管理系统-平台属性管理动态展示属性

--- vue-admin-template-master/src/views/product/Attr/index.vue
+++ vue-admin-template-master/src/views/product/Attr/index.vue
@@ -1,50 +1,79 @@
 <template>
   <div>
-    <el-card style="margin:20px 0;">
+    <el-card style="margin: 20px 0">
       <category-select @getCategoryId="getCategoryId"></category-select>
     </el-card>
     <el-card>
-      
+      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+      <!-- 表格：展示平台属性 -->
+      <el-table style="width: 100%" border :data="attrList">
+        <el-table-column type="index" label="序号" width="80" align="center">
+        </el-table-column>
+        <el-table-column prop="attrName" label="属性名称" width="150">
+        </el-table-column>
+        <el-table-column prop="prop" label="属性值列表" width="width">
+          <template slot-scope="{row,$index}">
+            <el-tag type="success" v-for="(attrValue,index) in row.attrValueList" :key="attrValue.id" style="margin: 5px 10px;">{{attrValue.valueName}}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="prop" label="操作" width="150">
+          <template slot-scope="{row,$index}">
+            <el-button type="warning" icon="el-icon-edit" size="mini" @click=""></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name:'Attr',
-  data () {
+  name: "Attr",
+  data() {
     return {
-      category1Id:'',
-      category2Id:'',
-      category3Id:''
-    }
+      category1Id: "",
+      category2Id: "",
+      category3Id: "",
+      // 接收平台属性的字段
+      attrList: [],
+    };
   },
   methods: {
     // 自定义事件的回调
-    getCategoryId({categoryId,level}){
-      console.log(level)
+    getCategoryId({ categoryId, level }) {
+      console.log(level);
       // 区分三级分类相应的id，以及父组件进行存储
-      if(level == 1){
+      if (level == 1) {
         this.category1Id = categoryId;
-        this.category2Id = ''
-        this.category3Id = ''
-      }else if(level == 2){
+        this.category2Id = "";
+        this.category3Id = "";
+      } else if (level == 2) {
         this.category2Id = categoryId;
-        this.category3Id = ''
-      }else{
+        this.category3Id = "";
+      } else {
         // 代表三级分类的id有了
         this.category3Id = categoryId;
-        // 发请求获取品牌属性
+        // 发请求获取平台属性的数据
         this.getAttrList();
       }
     },
 
     // 获取平台属性的数据
-    getAttrList(){
-      console.log('发请求')
+    // 当用用户确定三级分类的数据时候，可以向服务器发请求获取平台属性进行展示
+    async getAttrList() {
+      const { category1Id, category2Id, category3Id } = this;
+      let result = await this.$API.attr.reqAttrList(
+        category1Id,
+        category2Id,
+        category3Id
+      );
+      if (result.code == 200) {
+        this.attrList = result.data;
+      }
     },
   },
-}
+};
 </script>
 
 <style scoped>
