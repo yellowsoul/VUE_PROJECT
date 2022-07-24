211f115e8ab13344cba93fe5097c1662a86695fa
yellowsoul <517878555@qq.com>
2022-07-24 18:20:05 +0800

    P138 025-尚硅谷-后台管理系统-平台属性之添加属性与修改属性静态完成

--- vue-admin-template-master/src/views/product/Attr/index.vue
+++ vue-admin-template-master/src/views/product/Attr/index.vue
@@ -3,26 +3,71 @@
     <el-card style="margin: 20px 0">
       <category-select @getCategoryId="getCategoryId"></category-select>
     </el-card>
+
     <el-card>
-      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
-      <!-- 表格：展示平台属性 -->
-      <el-table style="width: 100%" border :data="attrList">
-        <el-table-column type="index" label="序号" width="80" align="center">
-        </el-table-column>
-        <el-table-column prop="attrName" label="属性名称" width="150">
-        </el-table-column>
-        <el-table-column prop="prop" label="属性值列表" width="width">
-          <template slot-scope="{row,$index}">
-            <el-tag type="success" v-for="(attrValue,index) in row.attrValueList" :key="attrValue.id" style="margin: 5px 10px;">{{attrValue.valueName}}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="prop" label="操作" width="150">
-          <template slot-scope="{row,$index}">
-            <el-button type="warning" icon="el-icon-edit" size="mini" @click=""></el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <div v-show="isShowTable">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!category3Id"
+          @click="isShowTable = false"
+          >添加属性</el-button
+        >
+        <!-- 表格：展示平台属性 -->
+        <el-table style="width: 100%" border :data="attrList">
+          <el-table-column type="index" label="序号" width="80" align="center">
+          </el-table-column>
+          <el-table-column prop="attrName" label="属性名称" width="150">
+          </el-table-column>
+          <el-table-column prop="prop" label="属性值列表" width="width">
+            <template slot-scope="{ row, $index }">
+              <el-tag
+                type="success"
+                v-for="(attrValue, index) in row.attrValueList"
+                :key="attrValue.id"
+                style="margin: 5px 10px"
+                >{{ attrValue.valueName }}</el-tag
+              >
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="150">
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+                @click="isShowTable = false"
+              ></el-button>
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+
+      <!-- 添加属性|修改属性的结构 -->
+      <div v-show="!isShowTable">
+        <el-form :inline="true" ref="form" label-width="80px">
+          <el-form-item label="属性名">
+            <el-input placeholder="请输入属性名"></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" icon="el-icon-puls" @click="">添加属性值</el-button>
+        <el-button  @click="isShowTable = true">取消</el-button>
+        <el-table style="width: 100%;margin:20px 0;" border>
+          <el-table-column align="center" type="index" label="序号" width="80">
+          </el-table-column>
+          <el-table-column  width="width" prop="prop" label="属性值名称">
+          </el-table-column>
+          <el-table-column  width="width" prop="prop" label="操作">
+          </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowTable = true">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -37,6 +82,8 @@ export default {
       category3Id: "",
       // 接收平台属性的字段
       attrList: [],
+      // 这个属性是控制table表格显示与隐藏的
+      isShowTable: false,
     };
   },
   methods: {
