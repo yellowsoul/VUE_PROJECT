26386c9734b2091400090ca950d22e511a823f39
yellowsoul <517878555@qq.com>
2022-08-01 01:17:54 +0800

    P157 044-尚硅谷-后台管理系统-SpuForm组件数据展示与收集数据

--- vue-admin-template-master/src/views/product/Spu/SpuForm/index.vue
+++ vue-admin-template-master/src/views/product/Spu/SpuForm/index.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
-    <el-form ref="form" label-width="80px" :inline="false" size="normal">
+    <el-form ref="form" label-width="80px" :model="spu">
       <el-form-item label="SPU名称">
-        <el-input placeholder="SPU名称"></el-input>
+        <el-input placeholder="SPU名称" v-model="spu.spuName"></el-input>
       </el-form-item>
 
       <el-form-item label="品牌">
-        <el-select placeholder="请选择品牌" value="">
-          <el-option label="label" value="value"></el-option>
+        <el-select placeholder="请选择品牌" v-model="spu.tmId">
+          <el-option :label="tm.tmName" :value="tm.id" v-for="(tm, index) in tradeMarkList" :key="tm.id"></el-option>
         </el-select>
       </el-form-item>
 
       <el-form-item label="SPU描述">
-        <el-input type="textarea" placeholder="描述" rows="4"></el-input>
+        <el-input type="textarea" placeholder="描述" rows="4" v-model="spu.description"></el-input>
       </el-form-item>
 
       <el-form-item label="SPU图片">
+        <!-- 上传图片：action图片上传的地址 list-type:文件列表类型  on-preview:图片预览的时候会触发 on-remove:当删除图片的时候会触发
+          file-list
+        -->
         <el-upload
-          action="https://jsonplaceholder.typicode.com/posts/"
+          action="/dev-api/admin/product/upload"
           list-type="picture-card"
           :on-preview="handlePictureCardPreview"
           :on-remove="handleRemove"
+          :file-list="spuImageList"
         >
           <i class="el-icon-plus"></i>
         </el-upload>
@@ -48,7 +52,7 @@
 
       <el-form-item>
         <el-button type="primary">保存</el-button>
-        <el-button @click="$emit('changeScene',0)">取消</el-button>
+        <el-button @click="$emit('changeScene', 0)">取消</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -61,10 +65,50 @@ export default {
     return {
       dialogImageUrl: "",
       dialogVisible: false,
-      spu:{}, // 存储SPU信息属性
-      tradeMarkList:[], // 存储品牌的信息
-      spuImageList:[], //存储SPU图片的数据
-      saleAttrList:[], // 销售属性的数据
+      // spu属性初始化的时候是一个空的对象,在修改SPU的时候，会向服务器发请求，返回SPU信息（对象），在修改的螩可以利用服务器返回的这个对象收集最新的数据提交给服务器
+      // 添加spu，如果是添加SPU的时候并没有向服务器发请求，数据收集到哪里呀[SPU]，收集数据的时候有哪些字段呀，看文档
+      
+      spu: { // 存储SPU信息属性（初始化数据，因为添加要携带这些字段）
+        // 三级分类的id
+        category3Id: 0,
+        // 描述
+        description: "",
+        // spu名称
+        spuName: "",
+        // 品牌的id
+        tmId: 0,
+        // 收集SPU图片的信息
+        spuImageList: [
+          // {
+          //   id: 0,
+          //   imgName: "",
+          //   imgUrl: "",
+          //   spuId: 0,
+          // },
+        ],
+        // 平台属性与属性值收集
+        spuSaleAttrList: [
+          // {
+          //   baseSaleAttrId: 0,
+          //   id: 0,
+          //   saleAttrName: "",
+          //   spuId: 0,
+          //   spuSaleAttrValueList: [
+          //     {
+          //       baseSaleAttrId: 0,
+          //       id: 0,
+          //       isChecked: "",
+          //       saleAttrName: "",
+          //       saleAttrValueName: "",
+          //       spuId: 0,
+          //     },
+          //   ],
+          // },
+        ],
+      },
+      tradeMarkList: [], // 存储品牌的信息
+      spuImageList: [], //存储SPU图片的数据
+      saleAttrList: [], // 销售属性的数据
     };
   },
   methods: {
@@ -77,33 +121,40 @@ export default {
     },
 
     // 初始化SpuForm数据
-    async initSpuData(spu){
+    async initSpuData(spu) {
       // 获取SPU信息的数据
       let spuResult = await this.$API.spu.reqSpu(spu.id);
-      if(spuResult.code == 200){
+      if (spuResult.code == 200) {
+        // 在修改spu的时候，需要向服务发请求的，把服务器返回的数据（对象），赋值给spu属性
         this.spu = spuResult.data;
       }
 
       // 获取品牌的信息
       let tradeMarkResult = await this.$API.spu.reqTradeMarkList();
-      if(tradeMarkResult.code == 200){
+      if (tradeMarkResult.code == 200) {
         this.tradeMarkList = tradeMarkResult.data;
       }
 
       // 获取SPU图片的数据
       let spuImageResult = await this.$API.spu.reqSpuImageList(spu.id);
-      if(spuImageResult.code == 200){
-        this.spuImageList = spuImageResult.data;
+      if (spuImageResult.code == 200) {
+        let listArr = spuImageResult.data;
+        // 由于照片墙显示图片的数据需要是数组，数组里面的元素需要有name与url字段
+        // 所以需要把服务器的数据进行修改
+        listArr.forEach(item => {
+          item.name = item.imgName;
+          item.url = item.imgUrl;
+        });
+        // 把整理好的数据赋值给
+        this.spuImageList = listArr;
       }
 
       // 获取平台全部的销售属性
       let saleResult = await this.$API.spu.reqBaseSaleAttrList();
-      if(saleResult.code == 200){
+      if (saleResult.code == 200) {
         this.saleAttrList = saleResult.data;
       }
-    }
-
-    
+    },
   },
 };
 </script>
