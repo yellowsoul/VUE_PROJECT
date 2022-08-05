31abeb94a46f8347121ea96d4647d030ae76029f
yellowsoul <517878555@qq.com>
2022-08-05 23:21:35 +0800

    P173 060-尚硅谷-后台管理系统-sku模块数据展示与分页功能

--- vue-admin-template-master/src/api/product/sku.js
+++ vue-admin-template-master/src/api/product/sku.js
@@ -1,1 +1,6 @@
+// sku模块
 import request from '@/utils/request';
+
+// sku列表的接口
+// GET /admin/product/list/{page}/{limit}
+export const reqSkuList = (page, limit) => request({url:`/admin/product/list/${page}/${limit}`,method:'get'});
--- vue-admin-template-master/src/views/product/Sku/index.vue
+++ vue-admin-template-master/src/views/product/Sku/index.vue
@@ -1,18 +1,86 @@
 <template>
   <div>
-    Sku
+    <!-- 表格 -->
+    <el-table :data="records" border style="width: 100%">
+      <el-table-column type="index" label="序号" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="skuName" label="名称" width="width">
+      </el-table-column>
+      <el-table-column prop="skuDesc" label="描述" width="width">
+      </el-table-column>
+      <el-table-column label="默认图片" width="110">
+        <template slot-scope="{row, $index}">
+          <img :src="row.skuDefaultImg" style="width:80px;height:80px;">
+        </template>
+      </el-table-column>
+      <el-table-column prop="weight" label="重量(KG)" width="80">
+      </el-table-column>
+      <el-table-column prop="price" label="价格(元)" width="80">
+      </el-table-column>
+      <el-table-column prop="prop" label="操作" width="width">
+        <template slot-scope="{row, $index}">
+          <el-button type="warning" icon="el-icon-sort-down" size="mini"></el-button>
+          <el-button type="success" icon="el-icon-sort-up" size="mini"></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button type="info" icon="el-icon-info" size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      @current-change="getSkuList"
+      @size-change="sizeChange"
+      style="text-align:center;"
+      :current-page="page"
+      :page-sizes="[3, 5, 10]"
+      :page-size="limit"
+      layout="prev, pager, next, jumper, ->,sizes, total"
+      :total="total">
+    </el-pagination>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name:'Sku',
-  data () {
-    return {}
+  name: "Sku",
+  data() {
+    return {
+      page:1, // 代表当前第几页
+      limit:10, // 代表当前页面有几条数据
+      records:[], // 存储Sku列表的数据
+      total:0, // 存储分页器一共展示的数据
+    };
   },
-  
-  methods: {},
-}
+
+  // 组件挂载完毕
+  mounted(){
+    // 获取sku列表的方法
+    this.getSkuList();
+  },
+  methods: {
+    // 获取sku列表数据的方法
+    async getSkuList(pages = 1){
+      this.page = pages;
+      // 解构出默认的参数
+      const { page, limit } = this;
+      let result = await this.$API.sku.reqSkuList(page,limit);
+      if(result.code == 200){
+        this.total = result.data.total;
+        this.records = result.data.records;
+      }
+    },
+
+    // 每页显示条数
+    sizeChange(limit){
+      // 修改参数
+      this.limit = limit;
+      this.getSkuList();
+    }
+
+  },
+};
 </script>
 
 <style scoped>
