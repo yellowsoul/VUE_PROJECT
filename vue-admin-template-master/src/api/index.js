65498c004d9e0fed2f13424d1a90996ab80f4652
yellowsoul <517878555@qq.com>
2022-08-16 00:19:51 +0800

    P199 086-尚硅谷-后台管理系统-权限管理模块的串讲(添加权限管理静态页面,配置路由、配置API结课预习)

--- vue-admin-template-master/src/api/index.js
+++ vue-admin-template-master/src/api/index.js
@@ -4,10 +4,18 @@ import * as attr from './product/attr';
 import * as spu from './product/spu';
 import * as sku from './product/sku';
 
+// 引入权限相关的接口文件
+import * as user from './acl/user';
+import role from './acl/role';
+import permission from './acl/permission';
 // 对外暴露
 export default {
   trademark,
   attr,
   spu,
-  sku
+  sku,
+
+  user,
+  role,
+  permission
 }
--- vue-admin-template-master/src/router/index.js
+++ vue-admin-template-master/src/router/index.js
@@ -54,6 +54,56 @@ export const constantRoutes = [
       meta: { title: '首页', icon: 'dashboard' }
     }]
   },
+  
+  // 权限管理
+  {
+    name: 'Acl',
+    path: '/acl',
+    component: Layout,
+    redirect: '/acl/user/list',
+    meta: {
+      title: '权限管理',
+      icon: 'el-icon-lock'
+    },
+    children: [
+      {
+        name: 'User',
+        path: 'user/list',
+        component: () => import('@/views/acl/user/list'),
+        meta: {
+          title: '用户管理',
+        },
+      },
+      {
+        name: 'Role',
+        path: 'role/list',
+        component: () => import('@/views/acl/role/list'),
+        meta: {
+          title: '角色管理',
+        },
+      },
+      {
+        name: 'RoleAuth',
+        path: 'role/auth/:id',
+        component: () => import('@/views/acl/role/roleAuth'),
+        meta: {
+          activeMenu: '/acl/role/list',
+          title: '角色授权',
+        },
+        hidden: true,
+      },
+      {
+        name: 'Permission',
+        path: 'permission/list',
+        component: () => import('@/views/acl/permission/list'),
+        meta: {
+          title: '菜单管理',
+        },
+      },
+    ]
+  },
+  
+  // 商品管理
   {
     path: '/product',
     component:Layout,
